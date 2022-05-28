fde7765085834e767b0cdd54a99c127d55000099
Mahmud Qosim <qmahmud222@gmail.com>
2022-05-28 04:56:46 -0700

    Initial Commit

---
+++ public/js/main.js
@@ -0,0 +1,267 @@
+// Socketio
+const socket = io()
+let username
+let room
+let index = 0
+
+// Get dom elements
+// Home
+const home_div = document.querySelector(".home")
+const username_input = document.getElementById("username_input")
+const room_input = document.getElementById("room_input")
+const join_btn = document.getElementById("join_btn")
+
+// Wait
+const wait_div = document.querySelector(".wait")
+const roomid_text = document.getElementById("roomid_text")
+const players_text = document.getElementById("players_text")
+const leader_div = document.getElementById("leader_div")
+const category_select = document.getElementById("category_select")
+const time_select = document.getElementById("time_select")
+const questions_select = document.getElementById("questions_select")
+const startgame_btn = document.getElementById("startgame_btn")
+
+let time_selected = Number(time_select.value) * 1000
+let num_of_questions = Number(questions_select.value)
+
+// Question
+const question_div = document.querySelector(".question")
+const timer = document.getElementById("timer")
+const number_text = document.getElementById("number_text")
+const difficulty_text = document.getElementById("difficulty_text")
+const category_text = document.getElementById("category_text")
+const question_text = document.getElementById("question_text")
+const choice_buttons = document.getElementsByClassName("question__choice")
+
+// Scoreboard
+const leaderboard_div = document.querySelector(".leaderboard")
+const leaderboard_text = document.querySelector(".leaderboard__text")
+const leaderboard = document.querySelector(".leaderboard__tablebody")
+
+time_select.addEventListener("change", (e) => {
+  time_selected = Number(e.target.value) * 1000
+})
+
+questions_select.addEventListener("change", (e) => {
+  num_of_questions = Number(e.target.value)
+  document.querySelector(
+    ".num_of_questions"
+  ).textContent = ` / ${num_of_questions}`
+})
+
+// Make user join the room when join btn clicked
+join_btn.addEventListener("click", function () {
+  // Get info
+  username =
+    username_input.value.charAt(0).toUpperCase() + username_input.value.slice(1)
+  room = room_input.value
+
+  // Check if inputs are not empty
+  if (username == "" || room == "") {
+    alert("Please enter username and room id to play")
+  } else {
+    // Show waiting room
+    document.querySelector(".home").classList.add("hidden")
+    document.querySelector(".wait").classList.remove("hidden")
+
+    // Make join
+    socket.emit("user_joined_room", { username, room })
+  }
+})
+
+// Display or change waiting room content
+socket.on("display_wait", ({ room, users_in_room }) => {
+  // Display info
+  roomid_text.innerHTML = room
+  players_text.innerHTML = users_in_room.join(", ")
+
+  // Show start button if it's the first player
+  if (username == users_in_room[0]) {
+    leader_div.classList.remove("hidden")
+  }
+})
+
+// Start game when leader clicks on start btn
+startgame_btn.addEventListener("click", async function () {
+  const startButton = document.querySelector(".button.start")
+  // Emit socket to start game
+  socket.emit("ask_start_game", {
+    num_of_questions,
+    category_selected: category_select.value,
+  })
+
+  startButton.textContent = "Loading..."
+  startButton.disabled = true
+
+  setTimeout(async () => {
+    // Loop through all questions
+
+    startButton.textContent = "Start Game"
+    startButton.disabled = false
+
+    while (index < num_of_questions) {
+      // Ask for question
+      socket.emit("ask_question", index)
+      await sleep(time_selected - 2000)
+
+      // Ask for results
+      socket.emit("ask_results", index)
+      await sleep(2000)
+
+      // Ask for leaderboard
+      socket.emit("ask_leaderboard", index)
+      await sleep(3000)
+
+      // Go next
+      index++
+    }
+  }, 5000)
+  await sleep(time_selected - 2000)
+})
+
+// Display question
+socket.on(
+  "display_question",
+  ({ index, difficulty, category, question, all_answers, error }) => {
+    if (error) {
+      alert(error)
+      window.location.reload()
+      return
+    }
+    // Enable slide in animation
+    leaderboard_div.classList.remove("slide-in-right")
+    leaderboard_div.classList.add("slide-out-left")
+    question_div.classList.remove("slide-out-left")
+    question_div.classList.add("slide-in-right")
+
+    // Show question div
+    wait_div.classList.add("hidden")
+    leaderboard_div.classList.add("hidden")
+    question_div.classList.remove("hidden")
+
+    // Reset answer buttons
+    for (let i = 0; i < choice_buttons.length; i++) {
+      choice_buttons[i].disabled = false
+      choice_buttons[i].classList.remove("selected")
+      choice_buttons[i].classList.remove("incorrect")
+      choice_buttons[i].classList.remove("correct")
+    }
+
+    // Start timer
+    document.getElementById("timer").innerHTML = ""
+    var bar = new ProgressBar.Line(timer, {
+      strokeWidth: 1,
+      easing: "linear",
+      duration: time_selected - 2000,
+      color: "#eebbc3",
+      trailColor: "#586497",
+      trailWidth: 0.5,
+      svgStyle: { width: "100%", height: "100%" },
+    })
+    bar.animate(1.0)
+
+    // Display question
+    number_text.innerHTML = index + 1
+    difficulty_text.innerHTML =
+      difficulty.charAt(0).toUpperCase() + difficulty.slice(1)
+    if (index == num_of_questions) {
+      difficulty_text.innerHTML = "hidden"
+    }
+    category_text.innerHTML = (
+      category.charAt(0).toUpperCase() + category.slice(1)
+    ).replace("Entertainment:", "")
+    question_text.innerHTML = question
+    choice_buttons[0].innerHTML = all_answers[0]
+    choice_buttons[1].innerHTML = all_answers[1]
+    choice_buttons[2].innerHTML = all_answers[2]
+    choice_buttons[3].innerHTML = all_answers[3]
+  }
+)
+
+// Display results
+socket.on("display_results", ({ correct_answer, error }) => {
+  if (error) {
+    window.location.reload()
+    return
+  }
+
+  let choice = document.querySelector(".selected")
+  let answers = document.getElementsByClassName("question__choice")
+  if (choice != null && choice.id == correct_answer) {
+    choice.classList.add("correct")
+    // correct_sound.play();
+  } else if (choice != null) {
+    choice.classList.add("incorrect")
+    answers[correct_answer].classList.add("correct")
+    // wrong_sound.play();
+  } else {
+    answers[correct_answer].classList.add("correct")
+    // wrong_sound.play();
+  }
+})
+
+// Display leaderboard
+socket.on("display_leaderboard", ({ index, scores_in_room }) => {
+  let message = `Question ${index + 2}`
+  if (index == num_of_questions - 2) {
+    message = `Last question! Double points`
+  }
+  if (index == num_of_questions - 1) {
+    message = `${scores_in_room[0].user_name} won with ${scores_in_room[0].score} points.`
+
+    /* let leaderboard_sound = new this.Howl({
+      src: [`../sounds/leaderboard.wav`],
+      volume: 0.25,
+    });
+    leaderboard_sound.play(); */
+    confetti({
+      particleCount: 250,
+      startVelocity: 30,
+      spread: 360,
+      count: 200,
+      origin: {
+        y: 0.4,
+      },
+    })
+  }
+
+  leaderboard.innerHTML = ""
+  leaderboard_text.innerHTML = message
+  for (i = 0; i < scores_in_room.length; i++) {
+    const Template = `
+    <tr>
+    <td>${i + 1}</td>
+    <td>${scores_in_room[i].user_name}</td>
+    <td>${scores_in_room[i].score}</td>
+    </tr>
+    `
+    leaderboard.insertAdjacentHTML("beforeend", Template)
+  }
+
+  question_div.classList.remove("slide-in-right")
+  question_div.classList.add("slide-out-left")
+  leaderboard_div.classList.remove("slide-out-left")
+  leaderboard_div.classList.add("slide-in-right")
+
+  leaderboard_div.classList.remove("hidden")
+  question_div.classList.add("hidden")
+})
+
+// Send user answer to server when clicking answer
+function submitChoice(choice_id) {
+  // Show choice
+  document.getElementById(choice_id).classList.add("selected")
+
+  // Disable buttons
+  for (let i = 0; i < choice_buttons.length; i++) {
+    choice_buttons[i].disabled = true
+  }
+
+  // Send choice to server
+  socket.emit("user_sent_choice", choice_id)
+}
+
+// Delay function
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
---
+++ server.js
@@ -0,0 +1,166 @@
+const path = require("path")
+const http = require("http")
+const express = require("express")
+const fetch = require("node-fetch")
+
+const app = express()
+const server = http.createServer(app)
+const socketio = require("socket.io")
+const io = socketio(server)
+
+const {
+  add_user,
+  get_room_usernames,
+  remove_user,
+  add_ten_questions,
+  get_a_question,
+  get_last_question,
+  get_room_scores,
+  add_score,
+} = require("./rooms")
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")))
+
+// Run when client connects
+io.on("connection", (socket) => {
+  // When user joins room
+  socket.on("user_joined_room", ({ username, room }) => {
+    let current = add_user(socket.id, username, room)
+    let current_room = current.room
+    let current_user = current.user
+
+    let current_question
+    let current_room_scores
+    let current_index
+
+    let is_first_to_answer
+    var score_to_add
+
+    // Make user join the room
+    socket.join(room)
+
+    // Display waiting room message
+    io.in(room).emit("display_wait", {
+      room: current_room.room_name,
+      users_in_room: get_room_usernames(current_room),
+    })
+
+    // Remove user if he leaves
+    socket.on("disconnect", () => {
+      remove_user(current_user, current_room)
+
+      io.in(room).emit("display_wait", {
+        room: current_room.room_name,
+        users_in_room: get_room_usernames(current_room).join(", "),
+      })
+    })
+
+    // When leader starts game
+    socket.on("ask_start_game", (settings) => {
+      // Get ten questions from api
+
+      fetch(
+        `https://opentdb.com/api.php?amount=${settings.num_of_questions}&type=multiple&category=${settings.category_selected}`
+      )
+        .then((res) => {
+          return res.json()
+        })
+        .then(function (json) {
+          // Add the ten questions to the room
+          add_ten_questions(current_room, json.results)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    })
+
+    // When leader requests a question
+    socket.on("ask_question", (index) => {
+      // Get next question
+      current_question = get_a_question(current_room)
+      current_index = index
+      is_first_to_answer = true
+
+      if (current_question) {
+        // Display question
+        io.in(room).emit("display_question", {
+          index,
+          difficulty: current_question.difficulty,
+          category: current_question.category,
+          question: current_question.question,
+          all_answers: current_question.incorrect_answers,
+          error: null,
+        })
+      } else {
+        // Error
+        io.in(room).emit("display_question", {
+          index,
+          difficulty: null,
+          category: null,
+          question: null,
+          all_answers: null,
+          error: "Unable to fetch questions.",
+        })
+      }
+    })
+
+    // Receive user choice and add his score
+    socket.on("user_sent_choice", (choice_id) => {
+      let current_question = get_last_question(current_room)
+
+      // Calculate score
+      score_to_add = 0
+      if (choice_id == current_question.correct_answer) {
+        if (current_question.difficulty == "easy") {
+          score_to_add = 20
+        } else if (current_question.difficulty == "medium") {
+          score_to_add = 30
+        } else {
+          score_to_add = 40
+        }
+        if (is_first_to_answer) {
+          score_to_add += 10
+          is_first_to_answer = false
+        }
+        if (current_index == 9) {
+          score_to_add *= 2
+        }
+
+        // Add score to user
+        add_score(current_room, current_user.user_id, score_to_add)
+      }
+    })
+
+    // When leader requests a result
+    socket.on("ask_results", (index) => {
+      // Display result
+
+      if (current_question) {
+        io.in(room).emit("display_results", {
+          correct_answer: current_question.correct_answer,
+        })
+      } else {
+        io.in(room).emit("display_results", {
+          error: 'Create a new game',
+        })
+      }
+    })
+
+    // When leader requests a leaderboard
+    socket.on("ask_leaderboard", (index) => {
+      // Get next question
+      current_room_scores = get_room_scores(current_room)
+
+      // Display question
+      io.in(room).emit("display_leaderboard", {
+        index,
+        scores_in_room: current_room_scores,
+      })
+    })
+  })
+})
+
+// Start server on port
+const PORT = process.env.PORT || 3000
+server.listen(PORT)
